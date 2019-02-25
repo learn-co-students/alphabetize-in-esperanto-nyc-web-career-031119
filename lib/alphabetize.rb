d78c9fce95efd64e106daead93d6a471a3cd29e9
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-25 17:51:49 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ arr.sort_by do |phrase|
+   phrase.split("").map do |letter|
+
+  alphabet.index(letter)
+
+    end 
+ end 
 end
