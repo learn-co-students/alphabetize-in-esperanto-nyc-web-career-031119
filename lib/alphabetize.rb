c91ecb4f5c5c87444ac452b695781900ae352c14
J-Agens <johntagens@gmail.com>
2019-02-28 10:48:29 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
-def alphabetize(arr)
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(sentences_arr)
   # code here
+  sentences_arr.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
 end
