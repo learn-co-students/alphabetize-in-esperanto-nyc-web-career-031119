ed9916aa046d01eb29b2429089f6a873a6e07713
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-28 21:59:27 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,4 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|phrase|  phrase}
+  arr.sort_by {|phrase|  phrase.split("").map {|char| ESPERANTO_ALPHABET.index(char)} }
 end
