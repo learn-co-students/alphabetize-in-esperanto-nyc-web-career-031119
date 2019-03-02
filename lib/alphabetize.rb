a936353677ddd605f0eca9c7db4287a96ce85a12
Elaina <elainaseki@gmail.com>
2019-03-02 05:18:59 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do|word|
+   word.split("").collect do|letter|
+    alphabet.index(letter)
+   end
+  end
   # code here
 end
